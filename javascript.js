808ba940186e97544e6782ce02f48c5b9b0b76f7
dulakshanwije <52505196+dulakshanwije@users.noreply.github.com>
2021-05-13 20:54:11 +0530

    Add javascript.js

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function tigger() {
+    var clientWidth = document.getElementById('main-container').clientWidth;
+
+    var borderWidth = clientWidth * 0.008;
+
+    alert(borderWidth);
+    document.getElementById("circle").style.borderWidth = borderWidth.toString() + "px";
+    document.getElementById("footer-box").style.borderWidth = borderWidth.toString() + "px";
+    for (i = 1; i < 8; i++) {
+        var value = "frame" + i.toString();
+        document.getElementById(value).style.borderWidth = borderWidth.toString() + "px";
+    }
+}
