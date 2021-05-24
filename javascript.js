7b62fffa09d623b419915402085f4d3dddf23fdb
dulakshanwije <52505196+dulakshanwije@users.noreply.github.com>
2021-05-25 04:11:40 +0530

    Commited

--- javascript.js
+++ javascript.js
@@ -1,15 +1,16 @@
-function playMusic() {
+function musicPlay() {
+    alert("called");
     document.getElementById("audio-file").play();
 }
 
 function tigger() {
+
     document.getElementById("audio-file").play();
     var clientWidth = document.getElementById('main-container').clientWidth;
     var footerHeight = document.getElementById('footer-box').clientHeight;
 
     var borderWidth = clientWidth * 0.008;
     var footerFont = footerHeight * 0.5;
-    var popboxWidth = clientWidth * 0.5;
 
     document.getElementById("circle").style.borderWidth = borderWidth.toString() + "px";
     document.getElementById("footer-box").style.borderWidth = borderWidth.toString() + "px";
